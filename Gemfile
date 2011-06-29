6bac104c46452002c322e968cd24bb75b4e6067e
grant <grant@debian.hsd1.pa.comcast.net>
2011-06-28 21:27:38 -0400

    Log4r stuff

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '~>3.0.9'
 gem 'rest-client'
+gem 'log4r'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Bestcrow::Application.initialize!
+
+#Log4r
+require 'log4r'
+require 'log4r/yamlconfigurator'
+
+require 'log4r/outputter/datefileoutputter'
+require 'log4r/outputter/emailoutputter'
+require 'log4r/outputter/udpoutputter'
+require 'log4r/formatter/log4jxmlformatter'
+
+log4r_config = Log4r::YamlConfigurator
+log4r_config["HOME"] = Rails.root.to_s
+log4r_config.load_yaml_file('config/log4r.yml')
