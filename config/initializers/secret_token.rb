c489e5ed30967bf7ae3baf9922af907d6f000372
grant <grant@debian.hsd1.pa.comcast.net>
2011-06-28 20:22:33 -0400

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bestcrow::Application.config.secret_token = '7831a9e91dbe13db232a7d54703f48eb820e963bfcc3511897238c964f4dae8f7774b4e817cbd20d43ed33de885c182c00a4603ca7864957773b28f35e040665'
