4d63313d3f210153a1066d6c4080292debfb6485
grant <kgo@grant-olson.net>
2011-07-06 20:09:11 -0400

    Go do deal page after creating deeal; allow arbitrary release amount

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -7,9 +7,9 @@ class DealsController < ApplicationController
     release_address = params[:deal][:release_address]
     #raise params.inspect
     deal_name, address = Bitcoind.new_deal current_user.email, release_address
-    Deal.create :user_id => current_user.id, :uuid => deal_name, :send_address => address, :release_address => release_address, :note => params[:deal][:note]
+    deal = Deal.create :user_id => current_user.id, :uuid => deal_name, :send_address => address, :release_address => release_address, :note => params[:deal][:note]
 
-    redirect_to deals_path
+    redirect_to deal_path deal.uuid
   rescue Bitcoind::BitcoindDown, Bitcoind::InvalidBitcoinAddress => ex
     flash.now[:alert] = "ERROR: " + ex.message
     @deals = Deal.find_all_by_user_id(current_user.id)
@@ -39,8 +39,8 @@ class DealsController < ApplicationController
   
   def release
     deal = Deal.find_by_uuid(params[:uuid])
-    coins = deal.line_item_balance
-    deal.release deal.line_item_balance
+    coins = params[:release_amount]
+    deal.release coins
     flash[:alert] = "Released #{coins} to #{deal.release_address}..."
     redirect_to deals_path
   rescue Bitcoind::BitcoindRefusedRequest => ex
